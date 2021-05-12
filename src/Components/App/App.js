74e8841b36b8f0244f62ee0253d5ca017dc5a380
Karen Garcia <saykaren@gmail.com>
2021-05-11 20:35:02 -0600

    Initial creation

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Styling/App.scss";
+// import { QueryClient, QueryClientProvider } from "react-query";
+
+
+// import {ReactQueryDevtools} from 'react-query/devtools';
+
+// const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+      <>
+      test
+      </>
+    // <QueryClientProvider client={queryClient}>
+
+    //   <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} />
+    // </QueryClientProvider>
+  );
+};
+
+export default App;
