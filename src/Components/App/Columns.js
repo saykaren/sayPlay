620985700f07a7f5dbfb57814805794bd06e2778
Karen Garcia <saykaren@gmail.com>
2021-05-15 09:11:47 -0600

    Column Filtering Column filtering can work with global filtering

--- src/Components/App/Columns.js
+++ src/Components/App/Columns.js
@@ -1,37 +1,45 @@
-import { format } from 'date-fns';
-
+import { format } from "date-fns";
+import ColumnFilter from "./ColumnFilter";
 
 export const COLUMNS = [
   {
     Header: "Id",
     Footer: "Id",
     accessor: "id",
+    Filter: ColumnFilter,
   },
   {
     Header: "First Name",
     Footer: "First Name",
     accessor: "first_name",
+    Filter: ColumnFilter,
   },
   {
     Header: "Last Name",
     Footer: "Last Name",
     accessor: "last_name",
+    Filter: ColumnFilter,
   },
   {
     Header: "Date of Birth",
     Footer: "Date of Birth",
     accessor: "date_of_birth",
-    Cell: ({ value }) =>{ return format(new Date(value), 'MM/dd/yyyy')}
+    Cell: ({ value }) => {
+      return format(new Date(value), "MM/dd/yyyy");
+    },
+    Filter: ColumnFilter,
   },
   {
     Header: "Country",
     Footer: "Country",
     accessor: "country",
+    Filter: ColumnFilter,
   },
   {
     Header: "Phone Number",
     Footer: "Phone Number",
     accessor: "phone",
+    Filter: ColumnFilter,
   },
 ];
 
@@ -42,8 +50,8 @@ export const GROUPED_COLUMNS = [
     accessor: "id",
   },
   {
-    Header: 'Name',
-    Footer: 'Name',
+    Header: "Name",
+    Footer: "Name",
     columns: [
       {
         Header: "First Name",
@@ -55,11 +63,11 @@ export const GROUPED_COLUMNS = [
         Footer: "Last Name",
         accessor: "last_name",
       },
-    ]
+    ],
   },
   {
-    Header: 'Info',
-    Footer: 'Info',
+    Header: "Info",
+    Footer: "Info",
     columns: [
       {
         Header: "Date of Birth",
@@ -76,6 +84,6 @@ export const GROUPED_COLUMNS = [
         Footer: "Phone Number",
         accessor: "phone",
       },
-    ]
+    ],
   },
-]
+];
