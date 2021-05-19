39b211bc96f0ce0515eeb14d57ad09f5313d2a1b
Karen Garcia <saykaren@gmail.com>
2021-05-18 20:30:30 -0600

    PageSize Client Side Shows the setPageSize function that you can put in the page size you want to set it to and the value is the pageSize you destructure from state

--- src/Components/App/PaginationTable.js
+++ src/Components/App/PaginationTable.js
@@ -3,11 +3,9 @@ import { useTable, usePagination } from "react-table";
 import MOCK_DATA from "./../Data/MOCK_DATA.json";
 import { COLUMNS, GROUPED_COLUMNS } from "./Columns";
 
-
 const PaginationTable = () => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
-
 
   const {
     getTableProps,
@@ -22,61 +20,90 @@ const PaginationTable = () => {
     state,
     gotoPage,
     pageCount,
+    setPageSize,
     prepareRow,
-  } = useTable({
-    columns,
-    data,
-    initialState: { pageIndex: 1}
-  }, usePagination);
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 1 },
+    },
+    usePagination
+  );
 
-  const { pageIndex } = state;
+  const { pageIndex, pageSize } = state;
   return (
-      <>
-    <table {...getTableProps()} id="table">
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {page.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}> {cell.render("Cell")}</td>;
-              })}
+    <>
+      <table {...getTableProps()} id="table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
-    <div>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
         <span>
-            Page{' '}
-            <strong>
-                {pageIndex + 1} of {pageOptions.length}
-            </strong>{' '}
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
         </span>
         <span>
-          | Go to page: {' '}
-          <input type='number' defaultValue={pageIndex + 1} 
-          onChange={(e)=>{
-            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
-            gotoPage(pageNumber)
-          }}
-          style={{width: '50px'}}
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
           />
         </span>
-        <button onClick={()=>gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
-        <button onClick={()=>previousPage()} disabled={!canPreviousPage}> Previous</button>
-       <button onClick={()=>nextPage()} disabled={!canNextPage}>Next</button>
-       <button onClick={()=>gotoPage(pageCount-1)} disabled={!canNextPage}>{'>>'}</button>
-    </div>
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {" "}
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
+      </div>
     </>
   );
 };
